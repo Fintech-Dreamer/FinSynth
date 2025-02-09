966d1f7aa1f047ccbd26051e97160fe0a33afabb
Eric <141918250+ProgramerEric123@users.noreply.github.com>
2025-02-09 20:07:39 +0800

    Update params.py

--- params.py
+++ params.py
@@ -2,3 +2,4 @@
 API_KEY = "sk-45MVSuMRHl3BQD8PIj6AxqgI2cflXAKqAMFZNyUIp1hWm0zy"
 API_BASE = "https://api.moonshot.cn/v1"
 MODEL = "moonshot-v1-8k"
+DS_API_KEY = " sk-ac3bffb2e9e84670a8130261a6c25fc6 "
