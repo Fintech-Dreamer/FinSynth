9ac872dd354c1744ff91f1d1b718d616b925be94
Str0keOOOO <786619108@qq.com>
2025-03-15 16:46:17 +0800

    暂时小小处理了一下XD

---
+++ src/lib/components/ChangelogModal.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { onMount, getContext } from 'svelte';
+	import { Confetti } from 'svelte-confetti';
+
+	import { WEBUI_NAME, config, settings } from '$lib/stores';
+
+	import { WEBUI_VERSION } from '$lib/constants';
+	import { getChangelog } from '$lib/apis';
+
+	import Modal from './common/Modal.svelte';
+	import { updateUserSettings } from '$lib/apis/users';
+
+	const i18n = getContext('i18n');
+
+	export let show = false;
+
+	let changelog = null;
+
+	onMount(async () => {
+		const res = await getChangelog();
+		changelog = res;
+	});
+</script>
